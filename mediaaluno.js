f21811f8a847c28b96dd0b0a3e297c2bccebddf1
Anderson Mendes <117383832+Minimixz@users.noreply.github.com>
2024-08-31 11:16:36 -0300

    Add files via upload

---
+++ mediaaluno.js
@@ -0,0 +1,15 @@
+var num = 4
+
+if (num > 7 && num < 10){
+    
+    console.log ('Foi', num,'Aprovado')
+}
+
+if (num >=5 && num <= 7){
+    console.log ('Foi', num, 'Aprovado')
+}
+
+
+else{
+    console.log ('Foi', num, 'Reprovado')
+}
