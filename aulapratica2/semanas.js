5c078c613266a3e37d6059b13a31017bc62346ce
Anderson Mendes <117383832+Minimixz@users.noreply.github.com>
2024-09-04 20:20:57 -0300

    Add files via upload

---
+++ aulapratica2/semanas.js
@@ -0,0 +1,39 @@
+var diaemingles = 'Tuesday'
+var diaemportugues
+
+switch(diaemingles){
+    case 'Monday':
+        diaemportugues='Segunda-feira'
+        break;
+    
+    case 'Tuesday':
+        diaemportugues='Terça-Feira'
+        break;
+    
+    case 'Wednesday':
+        diaemportugues='Quarta-Feira'
+        break;
+        
+    case 'Thursday':
+        diaemportugues='Quinta-Feira'
+        break;            
+        
+    case 'Friday':
+        diaemportugues='Sexta-Feira'
+        break;
+        
+    case 'Saturday':
+        diaemportugues='Sabado'
+        break;     
+    
+    case 'Sunday':
+        diaemportugues='Domingo'
+        break;
+    
+    default:
+        diaemportugues='Invalido'    
+
+   
+}
+
+console.log(diaemportugues)
