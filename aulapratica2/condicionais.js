25a49287cbf63bee99f941adbef84b458161c8e9
Anderson Mendes <117383832+Minimixz@users.noreply.github.com>
2024-09-04 20:05:46 -0300

    Add files via upload

---
+++ aulapratica2/condicionais.js
@@ -0,0 +1,16 @@
+var letra = 'a'
+
+switch (letra){
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+
+console.log('Vogal')
+break
+
+default:
+    console.log ('Consoante')
+
+}
